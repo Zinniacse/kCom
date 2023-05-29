4d66ff709749539298c7348c1ba82818279e5f0c
Zinniacse <zinnia.cse7862@gmail..com>
2023-05-29 15:55:05 +0600

    sidebar selection

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -15,11 +15,23 @@ describe('The Home Page', () => {
     //cy.visit('https://stage.ayersfood.com/');
       cy.get('.d-xl-none > .nav-item > .nav-link').click({ force: true });
       //  // Click on the cross icon to close the sidebar
-      //  cy.wait(2000);
-      //  cy.get('.nav-toggle').click(); // Replace with the actual selector for the cross icon
+    
       cy.get('.active > .d-flex').click();
-      cy.get('.content-header-title').contains('Home');
+      cy.get('.navigation > :nth-child(2) > [href="#"]').click();
 
+      cy.wait(2000); // Wait for 2 seconds before interacting with the element
+      cy.get('#__BVID__96 > :nth-child(1) > .d-flex').contains('User').click();
+      cy.get('#__BVID__96 > :nth-child(2) > .d-flex').contains('Roles').click();
+      cy.get('#__BVID__96 > :nth-child(3) > .d-flex').contains('User Roles').click();
+      
+      cy.get('.active > .d-flex').click();
+      cy.get('.navigation > :nth-child(3) > [href="#"]').click();
+      cy.wait(2000); // Wait for 2 seconds before interacting with the element
+      cy.get('#__BVID__109 > :nth-child(1) > .d-flex').contains('Suppliers').click();
+      cy.get('#__BVID__109 > :nth-child(2) > .d-flex').contains('Customers').click();
+      cy.get('#__BVID__109 > :nth-child(3) > .d-flex').contains('Customers Group').click();
+      cy.get('#__BVID__109 > :nth-child(4) > .d-flex').contains('Customers Report').click();
+      
   })
   
 })
