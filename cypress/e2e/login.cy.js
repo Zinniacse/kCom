cf666783e459adba8d9ebb4b7ce64bc9d5e15dd0
Zinniacse <zinnia.cse7862@gmail..com>
2023-05-30 18:02:36 +0600

    sidenavbar completed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -22,43 +22,37 @@ describe('The Home Page', () => {
         cy.get('.active > .d-flex').click();
         cy.get('.navigation > :nth-child(2) > [href="#"]').click();
   
-        //cy.wait(1000); // Wait for 2 seconds before interacting with the element
+        
         cy.get('#__BVID__96 > :nth-child(1) > .d-flex').contains('User').click();
         cy.url().should('include', 'https://stage.ayersfood.com/user-management/users/list'); // Check if the URL contains '/user
-        //cy.wait(1000); 
-  
         cy.get('#__BVID__96 > :nth-child(2) > .d-flex').contains('Roles').click();
         cy.url().should('include', 'https://stage.ayersfood.com/user-management/roles/list'); // Check if the URL contains '/roles'
-       // cy.wait(1000); 
   
         cy.get('#__BVID__96 > :nth-child(3) > .d-flex').contains('User Roles').click();
         cy.url().should('include', 'https://stage.ayersfood.com/user-management/user-roles/list'); // Check if the URL contains '/user-roles'
-        //cy.wait(1000); 
+ 
   
   
         /// Contacts
         cy.get('.active > .d-flex').click();
         cy.get('.navigation > :nth-child(3) > [href="#"]').click();
-      //  cy.wait(1000); // Wait for 2 seconds before interacting with the element
+     
   
         
         cy.get('#__BVID__109 > :nth-child(1) > .d-flex').contains('Suppliers').click();
         cy.url().should('include', 'https://stage.ayersfood.com/contacts/supplier/list'); // Check if the URL contains '/user-roles'
-      //  cy.wait(1000); 
-  
+     
         cy.get('#__BVID__109 > :nth-child(2) > .d-flex').contains('Customers').click();
         cy.url().should('include', 'https://stage.ayersfood.com/contacts/customer/list'); // Check if 
-     //   cy.wait(1000);
+
   
         cy.get('#__BVID__109 > :nth-child(3) > .d-flex').contains('Customers Group').click();
         cy.url().should('include', 'https://stage.ayersfood.com/contacts/customer-group/list'); // Check if 
-       // cy.wait(1000); 
-  
+      
        
         cy.get('#__BVID__109 > :nth-child(4) > .d-flex').contains('Customers Report').click();
         cy.url().should('include', 'https://stage.ayersfood.com/contacts/customer-report/list'); // Check if 
-       // cy.wait(1000); 
-  
+
   // Purchase
         cy.get('.active > .d-flex').click();
         cy.get('.navigation > :nth-child(4) > [href="#"]').click();
@@ -67,68 +61,65 @@ describe('The Home Page', () => {
         cy.wait(1000); // Wait for 2 seconds before interacting with the element
         cy.get('#__BVID__125 > :nth-child(1) > .d-flex').contains('Purchase Order Handle').click();
         cy.url().should('include', 'https://stage.ayersfood.com/purchase-order/create'); // Check if 
-      //  cy.wait(1000); 
-  
+    
         cy.get('#__BVID__125 > :nth-child(2) > .d-flex').contains('Purchase Order List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/purchase-order/list'); // Check if 
-     //   cy.wait(1000); 
+    
   
         cy.get('#__BVID__125 > :nth-child(3) > .d-flex').contains('Purchase Return Handle').click();
         cy.url().should('include', 'https://stage.ayersfood.com/purchase/purchase-return-handle'); // Check if 
-      //  cy.wait(1000); 
+    
   
         cy.get('#__BVID__125 > :nth-child(4) > .d-flex').contains('Purchase Return List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/purchase/purchase-return/list'); // Check if 
-      //  cy.wait(1000); 
+     
   
   
   // Sales
   
         cy.get('.active > .d-flex').click();
         cy.get('.navigation > :nth-child(5) > [href="#"]').click();
-      //  cy.wait(1000); // Wait for 2 seconds before interacting with the element
+  
   
   
         cy.get('#__BVID__141 > :nth-child(1) > .d-flex').contains('Quotation Handle').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/quotation-handle'); // Check if 
-      //  cy.wait(1000);
-  
+    
         cy.get('#__BVID__141 > :nth-child(2) > .d-flex').contains('Quotation List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/quotation-list'); // Check if 
-     //   cy.wait(1000);
+   
   
   
         cy.get('#__BVID__141 > :nth-child(3) > .d-flex').contains('Invoice Handle').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/invoice-handle'); // Check if 
-       // cy.wait(1000);
+    
   
   
   
         cy.get('#__BVID__141 > :nth-child(4) > .d-flex').contains('Invoice List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/invoice-list'); // Check if 
   
-      //  cy.wait(1000);
+     
   
   
   
         cy.get('#__BVID__141 > :nth-child(5) > .d-flex').contains('Sale Return Handle').click();
   
         cy.url().should('include', 'https://stage.ayersfood.com/sale/sale-return-handle'); // Check if 
-      //  cy.wait(1000);
+    
   
   
   
         cy.get('#__BVID__141 > :nth-child(6) > .d-flex').contains('Sale Return List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/sale-return/list'); // Check if 
-     //   cy.wait(1000);
-  
+
   
   
         cy.get('#__BVID__141 > :nth-child(7) > .d-flex').contains('Discount List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/discount-list'); // Check if 
   
         cy.get('#__BVID__141 > :nth-child(8) > .d-flex').contains('Reports').click();
-        // cy.url().should('include', 'https://stage.ayersfood.com/sale/reports/item-summary'); // Check if 
+      
         
         cy.get('#__BVID__166 > :nth-child(1) > .d-flex').contains('Item Summary').click();
         cy.url().should('include', 'https://stage.ayersfood.com/sale/reports/item-summary'); // Check if 
@@ -141,30 +132,30 @@ describe('The Home Page', () => {
   
         cy.get('.active > .d-flex').click();   
         cy.get('.navigation > :nth-child(6) > [href="#"]').click();
-       // cy.wait(1000); // Wait for 2 seconds before interacting with the element
+     
   
   
         cy.get('#__BVID__176 > :nth-child(1) > .d-flex').contains('Product List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/product/list'); // Check if 
-        cy.wait(1000);
+       
        
         cy.get('#__BVID__176 > :nth-child(2) > .d-flex').contains('Brands').click();
         cy.url().should('include', 'https://stage.ayersfood.com/products/brand/list'); // Check if 
-        //cy.wait(1000);
+      
   
        
         cy.get('#__BVID__176 > :nth-child(3) > .d-flex').contains('Units').click();
         cy.url().should('include', 'https://stage.ayersfood.com/products/unit/list'); // Check if 
-      //  cy.wait(1000);
+     
   
         cy.get('#__BVID__176 > :nth-child(4) > .d-flex').contains('Categories').click();
         cy.url().should('include', 'https://stage.ayersfood.com/products/category-list'); // Check if 
-      //  cy.wait(1000);
+   
   
       
   
         cy.get('#__BVID__176 > :nth-child(5) > .d-flex').contains('Reports').click();
-        // cy.url().should('include', 'https://stage.ayersfood.com/sale/reports/item-summary'); // Check if 
+        
         
         cy.get('#__BVID__192 > :nth-child(1) > .d-flex').contains('Product Report').click();
         cy.url().should('include', 'https://stage.ayersfood.com/products/report/product-report'); // Check if 
@@ -177,31 +168,46 @@ describe('The Home Page', () => {
   
         cy.get('.active > .d-flex').click();   
         cy.get('.navigation > :nth-child(7) > [href="#"]').click();
-     //   cy.wait(1000); // Wait for 2 seconds before interacting with the element
+   
   
   
         cy.get('#__BVID__202 > :nth-child(1) > .d-flex').contains('COA List').click();
         cy.url().should('include', 'https://stage.ayersfood.com/accounting/coa-list'); // Check if 
-    //    cy.wait(1000);
-       
+      
         cy.get('#__BVID__202 > :nth-child(2) > .d-flex').contains('New Payment').click();
         cy.url().should('include', 'https://stage.ayersfood.com/accounting/payment-receive-handle'); // Check if 
-   //     cy.wait(1000);
+
   
        
         cy.get('#__BVID__202 > :nth-child(3) > .d-flex').contains('Payment Receive').click();
         cy.url().should('include', 'https://stage.ayersfood.com/accounting/payment-receive-list'); // Check if 
-     //   cy.wait(1000);
+
   
         cy.get('#__BVID__202 > :nth-child(4) > .d-flex').contains('Undeposit Funds').click();
         cy.url().should('include', 'https://stage.ayersfood.com/accounting/undeposit-funds'); // Check if 
-     //   cy.wait(1000);
+
   
         cy.get('#__BVID__202 > :nth-child(5) > .d-flex').contains('Deposit Funds').click();
         cy.url().should('include', 'https://stage.ayersfood.com/accounting/deposit-funds'); // Check if 
-      //  cy.wait(1000);
+    
+
+      cy.get('.navigation > :nth-child(8) > .d-flex').contains('Bank').click();
+
+      cy.get('.navigation > :nth-child(9) > .d-flex').contains('Taxes').click();
+
+      cy.get('.navigation > :nth-child(10) > .d-flex').contains('Stores').click();
+
+      cy.get('.navigation > :nth-child(11) > .d-flex').contains('Warehouses').click();
   
-  
-        })
-      })
       
+      cy.get('.active > .d-flex').click();   
+      cy.get('.navigation > :nth-child(12) > [href="#"]').click();
+   
+      cy.get('#__BVID__233 > :nth-child(1) > .d-flex').contains('Business Settings').click();
+      cy.url().should('include', 'https://stage.ayersfood.com/products/report/product-report'); // Check if 
+       cy.get('#__BVID__233 > :nth-child(2) > .d-flex').contains('Reason List').click();
+       cy.url().should('include', 'https://stage.ayersfood.com/products/report/stock-summary'); // Check if 
+  
+    })
+  
+  })
