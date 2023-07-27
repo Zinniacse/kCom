637ece985ec60dff9efe51b70de679ca95fda9e2
Zinnia <zinnia.cse7862@gmail.com>
2023-07-27 18:22:08 +0600

    trading details added

--- cypress/e2e/customer_create.cy.js
+++ cypress/e2e/customer_create.cy.js
@@ -89,5 +89,14 @@ describe('User login', () => {
     // Typing the phone number
     cy.get('#add-customer-contact-details-mobile-679_phone_number').type('(02) 9876 5432');
     cy.get('.wizard-footer-right > span > .wizard-btn').click();
+
+
+    //Trading details
+    cy.get('#trading_name').type('pran co.');
+    cy.get('#trading-address-line-1').type('Sydney');
+    cy.get('[style="width: 518px; position: absolute; left: 856px; top: 394px;"] > :nth-child(1)').click();
+    cy.get('span > #email').type('pran.co@gmail.com');
+    cy.get('.wizard-footer-right > span > .wizard-btn').click();
+    
   });
 });
